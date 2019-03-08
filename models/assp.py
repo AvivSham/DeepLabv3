c7d2c936a42ca14b471448fa085c00ed485f2210
Arunava <learningdroidarunava@gmail.com>
2019-03-08 17:27:37 +0530

    Update assp.py

--- models/assp.py
+++ models/assp.py
@@ -2,82 +2,101 @@ import torch
 import torch.nn as nn
 
 class ASSP(nn.Module):
-  def __init__(self, nc,in_channels=1024, out_channels = 256):
+  def __init__(self,in_channels,out_channels = 256):
     super(ASSP,self).__init__()
     
-    self.nc = nc
     
-    self.bn = nn.BatchNorm2d(out_channels)
-    self.bnorm2 = nn.BatchNorm2d(out_channels)
-    self.relu = nn.ReLU()
-    
+    self.relu = nn.ReLU(inplace=True)
     
     self.conv1 = nn.Conv2d(in_channels = in_channels, 
                           out_channels = out_channels,
                           kernel_size = 1,
                           padding = 0,
-                          dilation=1)
+                          dilation=1,
+                          bias=False)
+    
+    self.bn1 = nn.BatchNorm2d(out_channels)
     
     self.conv2 = nn.Conv2d(in_channels = in_channels, 
                           out_channels = out_channels,
                           kernel_size = 3,
+                          stride=1,
                           padding = 6,
-                          dilation = 6)
+                          dilation = 6,
+                          bias=False)
+    
+    self.bn2 = nn.BatchNorm2d(out_channels)
     
     self.conv3 = nn.Conv2d(in_channels = in_channels, 
                           out_channels = out_channels,
                           kernel_size = 3,
+                          stride=1,
                           padding = 12,
-                          dilation = 12)
+                          dilation = 12,
+                          bias=False)
+    
+    self.bn3 = nn.BatchNorm2d(out_channels)
     
     self.conv4 = nn.Conv2d(in_channels = in_channels, 
                           out_channels = out_channels,
                           kernel_size = 3,
+                          stride=1,
                           padding = 18,
-                          dilation = 18)
+                          dilation = 18,
+                          bias=False)
+    
+    self.bn4 = nn.BatchNorm2d(out_channels)
     
     self.conv5 = nn.Conv2d(in_channels = in_channels, 
                           out_channels = out_channels,
                           kernel_size = 1,
+                          stride=1,
                           padding = 0,
-                          dilation=1)
+                          dilation=1,
+                          bias=False)
+    
+    self.bn5 = nn.BatchNorm2d(out_channels)
     
     self.convf = nn.Conv2d(in_channels = out_channels * 5, 
-                          out_channels = nc,
+                          out_channels = out_channels,
                           kernel_size = 1,
+                          stride=1,
                           padding = 0,
-                          dilation=1)
+                          dilation=1,
+                          bias=False)
     
-    self.adapool = nn.AdaptiveAvgPool2d((16, 16))
+    self.bnf = nn.BatchNorm2d(out_channels)
+    
+    self.adapool = nn.AdaptiveAvgPool2d(1)  
    
   
   def forward(self,x):
     
     x1 = self.conv1(x)
-    x1 = self.bn(x1)
+    x1 = self.bn1(x1)
     x1 = self.relu(x1)
     
     x2 = self.conv2(x)
-    x2 = self.bn(x2)
+    x2 = self.bn2(x2)
     x2 = self.relu(x2)
     
     x3 = self.conv3(x)
-    x3 = self.bn(x3)
+    x3 = self.bn3(x3)
     x3 = self.relu(x3)
     
     x4 = self.conv4(x)
-    x4 = self.bn(x4)
+    x4 = self.bn4(x4)
     x4 = self.relu(x4)
     
     x5 = self.adapool(x)
     x5 = self.conv5(x5)
-    x5 = self.bn(x5)
+    x5 = self.bn5(x5)
     x5 = self.relu(x5)
+    x5 = F.interpolate(x5, size = tuple(x4.shape[-2:]), mode='bilinear')
     
-    print (x1.shape, x2.shape, x3.shape, x4.shape, x5.shape)
     x = torch.cat((x1,x2,x3,x4,x5), dim = 1) #channels first
     x = self.convf(x)
-    x = self.bnorm2(x)
+    x = self.bnf(x)
     x = self.relu(x)
     
     return x
